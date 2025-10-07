2af2dfd06b2acc448307044ee450953813afb3ca
Lukatami <izraylit@gmail.com>
2025-10-07 15:39:11 -0400

    PUT/PATCH method added / Postman tested

--- routes/r-players.js
+++ routes/r-players.js
@@ -17,22 +17,66 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const { name: name, class: playerClass, level = 1 } = req.body;
+  // Sample body for POST request { "name": "TestName", "class": "TestClass" }
 
-  if (!name || !playerClass) {
+  const { name: playerName, class: className, level: playerLevel } = req.body;
+
+  if (!playerName || !className) {
     return res.status(400).json({ error: "Name and class required" });
   }
 
   const newPlayer = {
     id: players.length ? players[players.length - 1].id + 1 : 1,
-    name: name,
-    class: playerClass,
-    level,
+    name: playerName,
+    class: className,
+    level: Number(playerLevel) ? Number(playerLevel) : 1,
   };
 
   players.push(newPlayer);
 
   res.status(201).json(newPlayer);
+});
+
+router.put("/:id", (req, res) => {
+  // Sample body for PUT request { "name": "Bob's brother", "class": "The Builder", "level": "13"}
+  const id = Number(req.params.id);
+  const { name: playerName, class: playerClass, level: playerLevel } = req.body;
+
+  const player = players.find((p) => p.id === id);
+
+  if (!player) {
+    return res.status(404).json({ error: "Player not found" });
+  }
+
+  if (!playerName || !playerClass || !playerLevel) {
+    return res
+      .status(400)
+      .json({ error: "All fields (name, class, level) required" });
+  }
+
+  player.name = playerName;
+  player.class = playerClass;
+  player.level = Number(playerLevel);
+
+  res.json(player);
+});
+
+router.patch("/:id", (req, res) => {
+  // Sample body for PATCH request { "name": "Bilbo Baggins", "class": "Hobbit", "level": "10"}
+  const id = Number(req.params.id);
+  const player = players.find((p) => p.id === id);
+
+  if (!player) {
+    return res.status(404).json({ error: "Player not found" });
+  }
+
+  const { name: playerName, class: playerClass, level: playerLevel } = req.body;
+
+  if (playerName !== undefined) player.name = playerName;
+  if (playerClass !== undefined) player.class = playerClass;
+  if (playerLevel !== undefined) player.level = Number(playerLevel);
+
+  res.json(player);
 });
 
 export default router;
