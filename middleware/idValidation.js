6f83bee3ee90ac56f2d50c8b8bc06acc3d359682
Lukatami <izraylit@gmail.com>
2025-10-08 21:00:27 -0400

    query params added to GET items (quality, type, playerId, minValue, maxValue, sort)

--- middleware/idValidation.js
+++ middleware/idValidation.js
@@ -1,14 +1,17 @@
 // Middleware of ID validation in GET / PUT / PATCH / DELETE requests (if ID specified)
 export default function idValidation(req, res, next) {
+  // All data in HTTP requests recieving as a String
+  // Convert recieved data to a number
   const numId = Number(req.params.id);
 
-  // All in HTTP requests recieving as String, to manipulate with correct numeral id we must validate it for Integer
+  // Check if it's actually a Integer and positive
   if (!Number.isInteger(numId) || numId <= 0) {
     return res.status(400).json({
-      error: `ID for ${req.method} ${req.originalUrl} must be a positive number`,
+      error: `ID for ${req.method} ${req.originalUrl} must be a positive Integer`,
     });
   }
 
+  // Param with validId for all requests using this middleware
   req.validId = numId;
 
   next();
--- routes/r-items.js
+++ routes/r-items.js
@@ -4,9 +4,68 @@ import idValidation from "../middleware/idValidation.js";
 
 const router = express.Router();
 
-// Base endpoing GET request
+// Base endpoing GET request with query params
+// http://localhost:3000/items?quality=rare&type=equipment&playerId=2&minValue=10&maxValue=100&sort=asc
 router.get("/", (req, res) => {
-  res.json(items);
+  let filteredItems = items;
+
+  const { quality, type, playerId, minValue, maxValue, sort } = req.query;
+
+  // http://localhost:3000/items?quality=rare
+  if (quality) {
+    filteredItems = filteredItems.filter(
+      (i) => i.quality.toLowerCase() === quality.toLowerCase()
+    );
+  }
+
+  // http://localhost:3000/items?type=consumable
+  if (type) {
+    filteredItems = filteredItems.filter(
+      (i) => i.type.toLowerCase() === type.toLowerCase()
+    );
+  }
+
+  // http://localhost:3000/items?playerId=1
+  if (playerId) {
+    const numPlayerId = Number(playerId);
+    if (!Number.isInteger(numPlayerId) || numPlayerId <= 0) {
+      return res
+        .status(400)
+        .json({ error: "playerId must be a positive integer" });
+    }
+    filteredItems = filteredItems.filter(
+      (item) => item.playerId === numPlayerId
+    );
+  }
+
+  // http://localhost:3000/items?minValue=100
+  if (minValue) {
+    let numMin = Number(minValue);
+    if (!Number.isInteger(numMin) || numMin <= 0) {
+      return res
+        .status(400)
+        .json({ error: "minValue must be a positive integer" });
+    }
+    filteredItems = filteredItems.filter((i) => i.value >= numMin);
+  }
+
+  // http://localhost:3000/items?maxValue=12
+  if (maxValue) {
+    let numMax = Number(maxValue);
+    if (!Number.isInteger(numMax) || numMax <= 0) {
+      return res
+        .status(400)
+        .json({ error: "minValue must be a positive integer" });
+    }
+    filteredItems = filteredItems.filter((i) => i.value <= numMax);
+  }
+
+  // http://localhost:3000/items?minValue=5&maxValue=100&sort=asc
+  if (sort === "asc") filteredItems.sort((a, b) => a.value - b.value);
+  // http://localhost:3000/items?minValue=5&maxValue=100&sort=desc
+  if (sort === "desc") filteredItems.sort((a, b) => b.value - a.value);
+
+  res.json(filteredItems);
 });
 
 // GET request with id param
